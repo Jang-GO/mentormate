108970d47f09ad8c2f8e42fec8dafcd77c601eeb
Jang-GO <jang8195@naver.com>
2025-01-24 13:09:06 +0900

    initial commit

---
+++ src/main/java/org/janggo/mentormate/MentormateApplication.java
@@ -0,0 +1,13 @@
+package org.janggo.mentormate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentormateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentormateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/janggo/mentormate/MentormateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.janggo.mentormate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentormateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
