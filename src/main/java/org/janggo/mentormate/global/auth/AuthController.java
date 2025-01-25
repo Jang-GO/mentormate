e6d287b35075a2ed617606bc960584c78b2e3179
Jang-GO <jang8195@naver.com>
2025-01-25 22:31:03 +0900

    Feat : 카카오 로그인 구현

--- src/main/java/org/janggo/mentormate/global/auth/AuthController.java
+++ src/main/java/org/janggo/mentormate/global/auth/AuthController.java
@@ -2,6 +2,7 @@ package org.janggo.mentormate.global.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,18 +15,25 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
-    private final KakaoUtil kakaoUtil;
+    private final AuthService authService;
 
     @GetMapping("/login")
     public ResponseEntity<Map<String, String>> getClientAndRedirect(){
-        Map<String, String> body = new HashMap<>();
-        body.put("client-id",kakaoUtil.getClient());
-        body.put("redirect-url", kakaoUtil.getRedirectUrl());
-        return ResponseEntity.ok().body(body);
+        return ResponseEntity.ok().body(authService.getClientAndRedirect());
     }
 
-//    @GetMapping("/login/kakao")
-//    public ResponseEntity<?> kakaoLogin(@RequestParam("code") String code){
-//
-//    }
+    @GetMapping("/login/kakao")
+    public ResponseEntity<Map<String, Object>> kakaoLogin(@RequestParam("code") String code){
+        // 1. 인증 코드로 액세스 토큰 요청
+        String accessToken = authService.getAccessToken(code);
+
+        // 2. 액세스 토큰으로 사용자 정보 요청
+        Map<String, Object> userInfo = authService.getUserInfo(accessToken);
+
+        // 3. 사용자 정보를 포함한 성공 응답 반환
+        return ResponseEntity.ok(Map.of(
+                "message", "로그인 성공",
+                "userInfo", userInfo
+        ));
+    }
 }
--- src/main/java/org/janggo/mentormate/global/auth/KakaoUtil.java
+++ src/main/java/org/janggo/mentormate/global/auth/KakaoUtil.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Getter
-@Component  // Spring 관리 빈으로 등록
+@Component
 public class KakaoUtil {
 
     @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
@@ -13,4 +13,10 @@ public class KakaoUtil {
 
     @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
     private String redirectUrl;
+
+    @Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
+    private String tokenUri;
+
+    @Value("${spring.security.oauth2.client.provider.kakao.user-info-uri}")
+    private String userInfoUri;
 }
